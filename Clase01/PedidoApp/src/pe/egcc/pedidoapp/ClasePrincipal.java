af2e70c18d4f73eb088bab0e7d1f0aa3bf11d575
Gustavo Coronel <gcoronelc@gmail.com>
2016-10-01 20:31:36 -0500

    Clase 01

---
+++ Clase01/PedidoApp/src/pe/egcc/pedidoapp/ClasePrincipal.java
@@ -0,0 +1,17 @@
+package pe.egcc.pedidoapp;
+
+import pe.egcc.pedidoapp.view.PedidoView;
+
+/**
+ *
+ * @author Gustavo Coronel
+ * @blog gcoronelc.blogspot.com
+ * @email gcoronelc@gmail.com
+ */
+public class ClasePrincipal {
+
+    public static void main(String[] args) {
+      PedidoView.main(args);
+    }
+    
+}
---
+++ Clase01/PedidoApp/src/pe/egcc/pedidoapp/service/PedidoService.java
@@ -0,0 +1,25 @@
+package pe.egcc.pedidoapp.service;
+
+/**
+ *
+ * @author Gustavo Coronel
+ * @blog gcoronelc.blogspot.com
+ * @email gcoronelc@gmail.com
+ */
+public class PedidoService {
+  
+  private final double IGV = 0.18;
+  
+  public double calcularImpuesto(double importe){
+    double impuesto;
+    impuesto = importe * IGV;
+    return impuesto;
+  }
+  
+  public double calcularTotal(double importe){
+    double total;
+    total = importe + calcularImpuesto(importe);
+    return total;
+  }
+  
+}
